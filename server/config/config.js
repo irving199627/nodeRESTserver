d73d81f411a3b6eedf6117bd563c41ca4589a03e
irving199627 <irving_rosado01@hotmail.com>
2019-03-07 10:07:05 -0600

    variable de entorno Mongo

--- server/config/config.js
+++ server/config/config.js
@@ -15,7 +15,7 @@ let urlDB;
 if (process.env.NODE_ENV === 'dev') {
     urlDB = 'mongodb://localhost:27017/cafe';
 } else {
-    urlDB = 'mongodb+srv://irosado:rZcrNxed9XC6jgr@cluster0-iusip.mongodb.net/cafe'
+    urlDB = process.env.MONGO_URL;
 }
 
 process.env.urlDB = urlDB;
